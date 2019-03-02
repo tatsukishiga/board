0b43c8c2985ec9f763ab307571210edd2e5c5626
shigatatsuki <pandaman8899@gmail.com>
2019-03-02 12:20:36 +0900

    Userモデルを作成
    モデルを作成し、パスワード暗号化時に必要な文とバリデーション、アソシエーションを追加した。

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, {presence: true}
+  validates :email, {presence: true, uniqueness: true}
+
+  has_many :topics
+  has_many :comments
+end
