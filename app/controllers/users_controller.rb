d5802a3ee471fb71c66f29e103664da8d9136476
shigatatsuki <pandaman8899@gmail.com>
2019-03-02 12:44:45 +0900

    ユーザーのsession機能を作成
    ユーザーコントローラーを作成し、新規登録、ログイン、ログアウト機能を追加。アプリケーションコントローラーにはsessionに関係するメソッドを3つ追加。ビューではログインフォームと新規登録画面を追加し、各リンクを作成した。実行後の通知、エラーメッセージも表示されるようにした。

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      profile_image: "default_image.jpg",
+      password: params[:password],
+      profile: "よろしくお願いします。"
+    )
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
+  end
+
+  def login_form
+  end
+
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to("/topics/index")
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]
+      render("users/login_form")
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # users
+  get 'signup' => 'users#new'
+  post 'users/create' => 'users#create'
+  get 'login' => 'users#login_form'
+  post 'login' => 'users#login'
+  post 'logout' => 'users#logout'
+
 end
