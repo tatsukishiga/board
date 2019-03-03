d05a780ba4e1bd2b8e7ee59299c308cd347fce28
shigatatsuki <pandaman8899@gmail.com>
2019-03-03 10:35:07 +0900

    トピック作成機能を追加
    作成機能を追加し、リンクも追加した。

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,4 +2,22 @@ class TopicsController < ApplicationController
   def index
     @topics = Topic.all.order(created_at: :desc)
   end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(
+      title: params[:title],
+      content: params[:content],
+      user_id: @current_user.id
+    )
+    if @topic.save
+      flash[:notice] = "トピックを作成しました"
+      redirect_to("/topics/index")
+    else
+      render("topics/new")
+    end
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # topics
   get 'topics/index' => 'topics#index'
+  get 'topics/new' => 'topics#new'
+  post 'topics/create' => 'topics#create'
 end
