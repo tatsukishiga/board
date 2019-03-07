fbb14050934f7e78d3485e1467d5e820b97b7c66
shigatatsuki <pandaman8899@gmail.com>
2019-03-07 23:04:09 +0900

    ページネーションの追加
    トピック一覧ページにページネーションを追加した。1ページあたり5項目表示されるようにした。

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,6 @@
 class TopicsController < ApplicationController
   def index
-    @topics = Topic.all.order(created_at: :desc)
+    @topics = Topic.page(params[:page])
   end
 
   def new
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,6 @@
 class Topic < ApplicationRecord
+  paginates_per 5  # 1ページあたり5項目表示
+
   validates :title, {presence: true, length: {maximum: 40}}
   validates :content, {presence: true, length: {maximum: 180}}
 
