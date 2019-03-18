34d95719eb0f99c52d72d352abbe28f17c33a3e9
shigatatsuki <pandaman8899@gmail.com>
2019-03-18 21:43:57 +0900

    検索機能のHTTPメソッドを変更
    １，メソッドを使用し、post→getに変更２，前回の記述だとルーティングでshowが優先されて処理されエラーになるため、上にコードを移した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # topics
+  get 'topics/search_result' => 'topics#search_result'
   resources :topics
-  post 'topics/search_result' => 'topics#search_result'
 end
