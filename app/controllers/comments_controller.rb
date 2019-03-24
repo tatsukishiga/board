1510f2f49303b618105e403075e983a84f0e19a2
shigatatsuki <pandaman8899@gmail.com>
2019-03-24 23:20:40 +0900

    指摘事項修正
    １，コメント投稿失敗時のrender処理を書きましたが、まだ検証中です。
    ２，URL直打対策でbefore_actionを追加しました。
    ３，updateメソッドの重複を削除しました。
    ４，名前付きpathに統一しました。
    ５，createアクションで@current_user.comments.new(~~~)
    の形に修正しました。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user
+  before_action :ensure_correct_user, {only: [:edit]}
 
   def index
     @comments = Comment.all
   end
 
   def create
-    @comment = Comment.new(
+    @comment = @current_user.comments.new(
       content: params[:content],
-      user_id: @current_user.id,
       topic_id: params[:topic_id]
     )
-    @comment.save
-    redirect_to("/topics")
+    if @comment.save
+      flash[:notice] = "コメントを投稿しました"
+      redirect_to(topics_path)
+    else
+      render ("topics/show")
+    end
   end
 
   def edit
@@ -20,10 +25,9 @@ class CommentsController < ApplicationController
 
   def update
     @comment = Comment.find_by(id: params[:id])
-    @comment.update(content: params[:comment][:content])
     if @comment.update(content: params[:comment][:content])
       flash[:notice] = "コメントを編集しました"
-      redirect_to("/topics")
+      redirect_to(topics_path)
     else
       render("comments/edit")
     end
@@ -33,6 +37,13 @@ class CommentsController < ApplicationController
     @comment = Comment.find_by(id: params[:id])
     @comment.destroy
     flash[:notice] = "コメントを削除しました"
-    redirect_to("/topics")
+    redirect_to(topics_path)
+  end
+
+  def ensure_correct_user
+    if @current_user.comments.ids != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to(topics_path)
+    end
   end
 end
