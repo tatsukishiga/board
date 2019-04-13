ff9061c944f93465019e238ba6a7bb9e247b7a81
shigatatsuki <pandaman8899@gmail.com>
2019-04-13 22:14:19 +0900

    トピックの表示変更
    新しい順に変更。

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,7 @@ class TopicsController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @topics = Topic.page(params[:page])
+    @topics = Topic.page(params[:page]).order(created_at: "desc")
   end
 
   def new
