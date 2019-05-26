655e99f4efbd8392255aae01c8a6aa762f4f92b0
ShigaTatsuki <pandaman8899@gmail.com>
2019-05-26 22:47:42 +0900

    Update user.rb
    ユーザーが削除された際に、関連付けされている不要なトピックやコメントも同時に削除されるようにした。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX ,message: "形式が間違っています" }
 
-  has_many :topics
-  has_many :comments
-  
+  has_many :topics, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   def admin?
     admin_status == true
   end
